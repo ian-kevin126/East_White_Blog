f91540d00d92e58e169a93b6c5c2a8e46e5b4ae9
ian_kevin126 <ian_kevin126@126.com>
2021-03-13 23:25:36 +0800

    project init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    title: 'East_White\'s 前端小站',
+    base: "/East_White_Blog/",
+    themeConfig: {
+        logo: '/East_White_logo.png',
+        sidebarDepth: 1,
+        smoothScroll: true,
+        editLinks: true,
+        nav: [
+            { text: 'HTML&CSS', link: '/Html_Css/' },
+            { text: 'JavaScript', link: '/JavaScript/' },
+            { text: 'TypeScript', link: '/TypeScript/' },
+            { text: 'React', link: '/React/' },
+            { text: 'Webpack', link: '/Webpack/' },
+            { text: 'Flutter', link: '/Flutter/' },
+            { text: '数据结构与算法', link: '/Data_Algorithm/' },
+            { text: '面试', link: '/Interview/' },
+        ],
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,8 @@
+### East_White —— 学习不止，止于至善
+
+![图片](https://uploader.shimo.im/f/rAZjV08glM0D6JKr.png!thumbnail?fileGuid=tC3p6YyGtDRkVwkc)
+
+
+
+
+
